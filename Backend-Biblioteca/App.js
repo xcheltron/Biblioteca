90d9d5ce83ba90c710b410bde1048f535e6dc4ac
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-03-17 14:26:14 -0600

    Se agrego el archivo de configuraciones y la libreria de dotenv

--- Backend-Biblioteca/App.js
+++ Backend-Biblioteca/App.js
@@ -1,12 +1,13 @@
 import express from "express";
-const app = express();
-const PORT=3000
 
+import { PORT } from "./Configurations.js";
+
+const app = express();
 
 app.get('/', (req, res)=>{
     res.send("Hola mundo")
 })
 
 app.listen(PORT, () => {
-    console.log("Server escuchando en el puerto" + PORT)
+    console.log("Server escuchando en el puerto: " + PORT)
 })
