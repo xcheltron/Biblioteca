c9be6623b134aa350f32133fab71b4d1d2bb914b
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-03-17 15:33:09 -0600

    Se agrego el Queri de las tablas de la base de datos

---
+++ database/queries/Tables.sql
@@ -0,0 +1,51 @@
+/*CREATE DATABASE library*/
+
+USE library;
+
+CREATE TABLE Person(
+	IdPerson INT AUTO_INCREMENT PRIMARY KEY,
+	idRol INT,
+	_Name VARCHAR(50),
+	Phone_number VARCHAR(10),
+	Email VARCHAR(100),
+	_Password VARCHAR(100), 
+	FOREIGN KEY (idRol) REFERENCES Rol(IdRol)		
+);
+
+CREATE TABLE Rol(
+	IdRol INT AUTO_INCREMENT PRIMARY	KEY,
+	NameRol VARCHAR(30)
+);
+
+CREATE TABLE Book(
+	IdBook INT AUTO_INCREMENT PRIMARY KEY,
+	Author VARCHAR (50),
+	Name_Book VARCHAR(100),
+	publication_date DATE,
+	quantity INT	
+);
+
+CREATE TABLE Loan(
+	IdLoan INT AUTO_INCREMENT PRIMARY KEY,
+	idPerson INT,
+	loan_date DATE,
+	max_date_return DATE,
+	return_date DATE, 
+	FOREIGN KEY (idPerson) REFERENCES Person(IdPerson)		
+);
+
+
+CREATE TABLE Loan_Detail(
+	IdLoan_Detail INT AUTO_INCREMENT PRIMARY	KEY,
+	IdLoan INT,
+	FOREIGN KEY (IdLoan) REFERENCES loan(IdLoan)
+);
+
+CREATE TABLE Fines(
+	IdFine INT AUTO_INCREMENT PRIMARY KEY,
+	idLoan INT,
+	Price DOUBLE,
+	Liquidated BOOLEAN,
+	Liquidated_date DATE, 
+	FOREIGN KEY (idLoan) REFERENCES Loan(idLoan)		
+);
