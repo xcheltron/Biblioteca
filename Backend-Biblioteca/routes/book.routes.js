4ecf1fb89b99a1898ea987093cf3e31b15481c43
Xchel Uriel Pérez González <L20290857@cdguzman.tecnm.mx>
2026-03-26 11:50:15 -0600

    Se agrego la ruta de la consulta general de los libros

--- Backend-Biblioteca/routes/book.routes.js
+++ Backend-Biblioteca/routes/book.routes.js
@@ -1,8 +1,10 @@
-import { addBookController } from "../controllers/book.controller.js";
+import { addBookController, consultGeneralBookController } from "../controllers/book.controller.js";
 import express from "express"
 
 const router = express.Router()
 
 router.post('/',addBookController)
+
+router.get('/consulta', consultGeneralBookController)
 
 export default router
